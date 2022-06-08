1b123082fc717b6ede0eedeaa23f3e883ced2fef
daniel <mr.daniels.2025@gmail.com>
2022-06-08 15:58:20 -0600

    Add arrays

--- arrays.js
+++ arrays.js
@@ -3,10 +3,13 @@ var arrayString = ["string 1", "string 2", "sting 3", "string 4"];
 var numbers = [1,2,3,4];
 
 var boleans = [age = false, height = true, weight = false, eyes = true];
-console.log(boleans[0]);
+
 arrayString.pop();
 console.log(arrayString);
 numbers.push(1);
 console.log(numbers);
 boleans.slice(2,3);
 console.log(boleans);
+
+//index positions represent the physical numerical position inside in an array.
+console.log(boleans[0]);
