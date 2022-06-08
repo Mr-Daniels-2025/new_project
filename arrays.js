f560cd6a823765fed0b591be4cff573d98e03c2f
daniel <mr.daniels.2025@gmail.com>
2022-06-08 15:44:59 -0600

    Add arrays

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var arrayString = ["string 1", "string 2", "sting 3", "string 4"];
+
+var numbers = [1,2,3,4];
+
+var boleans = [age = false, height = true, weight = false, eyes = true];
+console.log(boleans[0]);
